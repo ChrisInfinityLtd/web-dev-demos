56b73752c4b1adc415eb1917d81c5e5303ad3aff
Chris Liu <xmacmail@gmail.com>
2025-08-28 16:07:33 +0800

    修正 vitepress 配置。

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
     title: "Web Dev Demos",
     description: "Demos and Docs",
+    ignoreDeadLinks: "localhostLinks",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
